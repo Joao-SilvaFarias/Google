77fdeebef16515b0b09b975ce6e8268bc144b26c
João Victor <joaovictorsilvafarias15@gmail.com>
2024-02-04 03:12:14 -0300

    Tema
    Adicionando o tema claro.

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
+const $html = document.querySelector('html');
+const $checkbox = document.querySelector('#switch');
 const clicar = document.querySelector('.barrapes');
 const passar = document.querySelector('.barrapes:hover');
 
+$checkbox.addEventListener('change', function(){
+    $html.classList.toggle('light-mode');
+})
+ 
 document.addEventListener('mousedown', (Event) => {
     if(clicar.contains(Event.target))  {
-        clicar.style.background = 'rgb(57, 57, 57)';
+        clicar.style.background = 'var(--barrapes-bg-hover)';
     }else{
         clicar.style.background = 'none';
         clicar.style = '.barrapes:hover';
     }
 })
+
+
+
+
